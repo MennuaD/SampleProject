b3a2fa22487d16f379124be6d089df76fd4ef4ce
MennuaD <mdavis0585@bths.edu>
2022-09-28 20:07:21 -0400

    Last

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mennua");
-        System.out.println("My favorite movie is Dragon Cry");
+        System.out.println("My name is Mennua.");
+        System.out.println("My favorite movie is Dragon Cry.");
+        System.out.println("My favorite food is falafel.");
     }
 }
